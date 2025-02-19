b01d599d193f57e8d21f761aaad2a2e6fe27c980
vijaykumar64 <165811870+vijaykumar64@users.noreply.github.com>
2025-02-19 18:53:52 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/": "https://ecommerce-e1n1.onrender.com",
+      "/uploads/": "https://ecommerce-e1n1.onrender.com",
     },
   },
 });
